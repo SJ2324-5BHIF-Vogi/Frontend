b902c6c9ae389119471c9fa38550fc8ccfeb4e96
FlameknightGD <FlameknightGD28@gmail.com>
2023-11-12 19:26:01 +0100

    feat: style searchbar

--- src/components/global/SearchBar.tsx
+++ src/components/global/SearchBar.tsx
@@ -1,11 +1,11 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import { RiSearchLine } from 'react-icons/ri'
 
 export const SearchBar: FC = () => {
   return (
-    <search className='flex items-center mt-5 rounded-full border'>
-      <RiSearchLine className='cursor-pointer' />
-      <input className='rounded-full' />
+    <search className='flex items-center mt-16 rounded-full mx-10 h-14 bg-zinc-950'>
+      <RiSearchLine className='cursor-pointer ml-4 text-2xl text-white' />
+      <input className='rounded-full text-white text-lg ml-3 bg-transparent' />
     </search>
   )
 }
