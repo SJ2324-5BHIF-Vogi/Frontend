fe055e9ebda6bf65587eeec7e046074b61675fee
FlameknightGD <FlameknightGD28@gmail.com>
2023-11-12 19:26:35 +0100

    feat: style layout

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement } from 'react'
 
 const Home = (): ReactElement => {
-  return <div className='flex flex-col m-[15px] space-y-[5px]'>Header</div>
+  return (
+    <div className='flex flex-col m-[15px] space-y-[5px] text-white'>
+      Content
+    </div>
+  )
 }
 
 export default Home
