65441d279e82f7a6b61a2a1ce9fe2014d494984c
FlameknightGD <FlameknightGD28@gmail.com>
2023-11-12 19:25:24 +0100

    feat: style sidebar

--- src/components/Sidebar/NavbarButton.tsx
+++ src/components/Sidebar/NavbarButton.tsx
@@ -1,4 +1,4 @@
-import { Component, FC } from 'react'
+import React, { Component, FC } from 'react'
 interface Props {
   children: string
   Icon: typeof Component
@@ -6,9 +6,9 @@ interface Props {
 
 export const NavbarButton: FC<Props> = ({ children, Icon }: Props) => {
   return (
-    <button className='flex items-center text-center border rounded-full'>
-      <Icon className='ml-3 mr-5' />
-      {children}
+    <button className='flex items-center text-center border rounded-full grow h-20 w-full'>
+      <Icon className='ml-8 mr-6 text-4xl' />
+      <span className='text-2xl'>{children}</span>
     </button>
   )
 }
--- src/views/Sidebar.tsx
+++ src/views/Sidebar.tsx
@@ -1,26 +1,31 @@
 import { NavbarButton } from '@/components/Sidebar/NavbarButton'
 import React, { FC } from 'react'
 import Image from 'next/image'
-import { RiHome2Line } from 'react-icons/ri'
+import {
+  RiCompassLine,
+  RiHome2Line,
+  RiUserLine,
+  RiVipDiamondLine
+} from 'react-icons/ri'
 
 export const Sidebar: FC = () => {
   return (
-    <nav className='flex flex-col px-1 overflow-hidden border-r-4 border-black grow w-1/4'>
-      <div className='my-5 mx-auto'>
+    <nav className='flex flex-col px-1 overflow-hidden border-r-4 bg-zinc-800 border-zinc-300 grow w-1/5 text-white'>
+      <div className='my-16 mx-auto'>
         <Image
           alt='Vogi Logo'
-          className='rounded-[75px]'
+          className='rounded-full'
           height={142}
           src='/vogi_logo.png'
           width={142}
         />
-        <p className='text-center my-3'>Vogi</p>
+        <p className='text-center my-4 text-3xl text-emerald-400'>Vogi</p>
       </div>
-      <div className='flex-col space-y-5 mx-auto'>
+      <div className='flex-col space-y-8 mx-auto grow w-2/3'>
         <NavbarButton Icon={RiHome2Line}>Home</NavbarButton>
-        <NavbarButton Icon={RiHome2Line}>Home</NavbarButton>
-        <NavbarButton Icon={RiHome2Line}>Home</NavbarButton>
-        <NavbarButton Icon={RiHome2Line}>Home</NavbarButton>
+        <NavbarButton Icon={RiCompassLine}>Explore</NavbarButton>
+        <NavbarButton Icon={RiUserLine}>Profile</NavbarButton>
+        <NavbarButton Icon={RiVipDiamondLine}>Premium</NavbarButton>
       </div>
     </nav>
   )
