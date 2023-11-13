827c8d51427db9f7209e252aa282109ad5fd0b37
FlameknightGD <FlameknightGD28@gmail.com>
2023-11-13 18:52:19 +0100

    feat: add hover color change to button

--- src/components/Sidebar/NavbarButton.tsx
+++ src/components/Sidebar/NavbarButton.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const NavbarButton: FC<Props> = ({ children, Icon }: Props) => {
   return (
-    <button className='flex items-center text-center border rounded-full grow h-20 w-full'>
+    <button className='flex items-center text-center rounded-full border grow h-20 w-full hover:text-emerald-400'>
       <Icon className='ml-8 mr-6 text-4xl' />
       <span className='text-2xl'>{children}</span>
     </button>
