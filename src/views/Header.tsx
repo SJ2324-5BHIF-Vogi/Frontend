84b57432cdb7f6f273c44a066fbe6d6ca629bef3
FlameknightGD <61699155+FlameknightGD@users.noreply.github.com>
2023-11-24 11:57:36 +0100

    feat: add user dynamic routes

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react'
 import Image from 'next/image'
 
-export const Header: FC = () => {
+interface Props {
+  username: string
+}
+
+export const Header: FC<Props> = ({ username }) => {
   return (
     <header className='flex border-b-4 bg-zinc-800 border-zinc-500 h-1/4 px-16 text-white font-manrope'>
       <div className='flex items-center grow w-1/2'>
@@ -14,7 +18,7 @@ export const Header: FC = () => {
         />
         <div className='flex flex-col ml-6'>
           <span className='text-2xl'>Vogi Official</span>
-          <span className='text-lg text-zinc-400'>@vogi</span>
+          <span className='text-lg text-zinc-400'>@{username}</span>
         </div>
       </div>
       <div className='flex items-center grow text-center w-1/2'>
