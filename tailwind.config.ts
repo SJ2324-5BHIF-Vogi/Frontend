b2ca74be3db3826b6d30d2a8105244a0246e9177
FlameknightGD <FlameknightGD28@gmail.com>
2023-11-27 13:53:26 +0100

    Implement neon mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
@@ -8,6 +9,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        'lilac-500': '#A875CF',
+        'midnight-700': '#1E1B2E',
+        'midnight-800': '#14121F',
+        'midnight-900': '#12101C'
       }
     },
     fontFamily: {
@@ -17,4 +24,5 @@ const config: Config = {
   },
   plugins: []
 }
+
 export default config
