94b1787ba68c54c61f97716b6a8934c09565bc32
FlameknightGD <61699155+FlameknightGD@users.noreply.github.com>
2024-01-10 11:42:22 +0100

    chore: reformat settings page structure

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { Sidebar } from '@/views/Sidebar'
+import { SearchBar } from '@/components/global/SearchBar'
+import { Metadata } from 'next'
+import React, { ReactElement, ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Vogi',
+  description: 'The only social media platform in the world.'
+}
+const RootLayout = ({ children }: { children: ReactNode }): ReactElement => {
+  return (
+    <>
+      <div>
+        <Sidebar />
+      </div>
+      <div className='flex bg-zinc-900 dark:bg-midnight-800 ml-auto mr-auto w-3/5 font-righteous'>
+        {children}
+      </div>
+      <aside className='border-gradient-l fixed right-[0px] h-screen bg-zinc-800 border-zinc-500 border-l-4 grow w-1/5 dark:bg-midnight-700'>
+        <SearchBar />
+      </aside>
+    </>
+  )
+}
+
+export default RootLayout
