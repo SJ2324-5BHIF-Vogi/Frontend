2c2103a38e3a222ba09b0cc470f7b8d89ee2cfdd
FlameknightGD <61699155+FlameknightGD@users.noreply.github.com>
2024-01-11 14:52:36 +0100

    feat: make settings page responsive

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -5,14 +5,14 @@ import { RiLock2Line, RiPaintBrushLine, RiUserLine } from 'react-icons/ri'
 const Settings: FC = () => {
   return (
     <>
-      <section className='flex flex-col grow w-1/3 items-center justify-center'>
-        <div className='w-2/3 space-y-8 font-righteous'>
+      <section className='flex flex-col grow 2xl:w-1/3 w-full items-center justify-center'>
+        <div className='2xl:w-2/3 w-3/4 space-y-8 font-righteous'>
           <NavbarButton Icon={RiUserLine}>Profile</NavbarButton>
           <NavbarButton Icon={RiPaintBrushLine}>Display</NavbarButton>
           <NavbarButton Icon={RiLock2Line}>Privacy</NavbarButton>
         </div>
       </section>
-      <section className='w-2/3'>Test</section>
+      <section className='2xl:w-2/3 w-full hidden 2xl:block'>Test</section>
     </>
   )
 }
