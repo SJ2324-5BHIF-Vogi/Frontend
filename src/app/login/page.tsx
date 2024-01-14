d939a2e22b017d5093f81279ba9f82e4412c851e
Fikri <91050135+Fikri-OK@users.noreply.github.com>
2024-01-14 13:46:45 +0100

    refactor: login page
    Create Account und Forget Password hinzugefügt, Borders für Textfelder abgeändert

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+//import { Link } from 'react-router-dom'
 
 const Login: FC = () => {
   return (
@@ -24,17 +25,22 @@ const Login: FC = () => {
               placeholder='Email address/Accountname'
             />
           </div>
-          <div className='mb-10'>
+          <div className='mb-6'>
             <input
               type='password'
               id='password'
-              className='w-full p-2 border border-white text-center bg-gray-500 rounded-full'
+              className='w-full p-2 border border-white text-center bg-gray-500 rounded-full mb-4'
               placeholder='Password'
             />
+            <button className='border border-white text-white p-2 rounded-full w-full h-full'>
+              Sign In
+            </button>
           </div>
-          <button className='border border-white text-white p-2 rounded-full w-full h-full'>
-            Sign In
-          </button>
+          <div className='text-white text-center'>
+            <label className='text-white'>Create Account</label>
+            <span className='mx-2'>|</span>
+            <label className='text-white'>Forgot Password?</label>
+          </div>
         </div>
       </div>
     </div>
